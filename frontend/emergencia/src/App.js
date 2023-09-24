d12cb5781d67b55a29f3b220e5badaaedf7196c8
AdolfoGzz <118020837+AdolfoGzz@users.noreply.github.com>
2023-09-24 06:09:44 -0600

    primera version tabla

--- frontend/emergencia/src/App.js
+++ frontend/emergencia/src/App.js
@@ -1,24 +1,34 @@
 import Header from "./components/Header";
-import NivelRiesgo from "./components/NivelRiesgo";
-import Emociones from "./components/Emociones";
-import Status from "./components/Status";
+// import NivelRiesgo from "./components/NivelRiesgo";
+// import Emociones from "./components/Emociones";
+// import Status from "./components/Status";
 import Box from '@mui/material/Box';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import Maps from "./components/Mapas/Mapa.js";
+import Table from "./components/Table.js";
+// import Maps from "./components/Mapa.js";
 
 
 function App() {
   return (
     <>
         <Header/>
-        <Box sx={{display:'flex',}}>
+        {/* <Box sx={{display:'flex',}}>
           <NivelRiesgo nivel = {0.4}/>
           <Emociones/>
           <Status/>
           <Maps/>
-        </Box>
+        </Box> */}
+        <Box sx={{
+          display:'flex',
+          width:1,
+          minHeight: 500,
+          justifyContent: 'center',
+          paddingTop: 5,
+          paddingX: 5,
+        }}><Table/></Box>
+
     </>
 
   );
---
+++ frontend/emergencia/src/components/Table.js
@@ -0,0 +1,49 @@
+// import Emociones from "./Emociones";
+// import Status from "./Status";
+// import Box from '@mui/material/Box';
+// import NivelRiesgo from "./NivelRiesgo";
+// import Maps from "./Mapa.js";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Typography } from '@mui/material';
+
+const tableHeaders = {
+    fontWeight: 600,
+    bgColor: '#E6E6E6',
+    align: 'center',
+}
+const cellCenter = {
+    textAlign: 'center',
+}
+
+function customTable() {
+  return (
+    <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 1000 }}>
+            <TableHead>
+                <TableRow sx={{
+                    backgroundColor: '#F4F4F4',
+                }}>
+                    <TableCell sx={cellCenter}><Typography style={tableHeaders}>Info del llamante</Typography></TableCell>
+                    <TableCell sx={cellCenter}><Typography style={tableHeaders}>Descripcion del problema</Typography></TableCell>
+                    <TableCell sx={cellCenter}><Typography style={tableHeaders}>Ubicacion</Typography></TableCell>
+                    <TableCell sx={cellCenter}><Typography style={tableHeaders}>Emociones</Typography></TableCell>
+                    <TableCell sx={cellCenter}><Typography style={tableHeaders}>Riesgo</Typography></TableCell>
+                    <TableCell sx={cellCenter}><Typography style={tableHeaders}>Status</Typography></TableCell>
+                    <TableCell sx={cellCenter}><Typography style={tableHeaders}>Llamada</Typography></TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                <TableRow></TableRow>
+            </TableBody>
+        </Table>
+    </TableContainer>
+  );
+}
+
+export default customTable;
