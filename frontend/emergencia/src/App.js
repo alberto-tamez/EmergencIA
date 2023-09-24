6d3fd3f7681455eebdf19caa3ee188086531ea19
AdolfoGzz <118020837+AdolfoGzz@users.noreply.github.com>
2023-09-24 02:08:15 -0600

    Add component NivelRiesgo

--- frontend/emergencia/src/App.js
+++ frontend/emergencia/src/App.js
@@ -1,10 +1,13 @@
 import Header from "./components/Header";
+import NivelRiesgo from "./components/NivelRiesgo";
 
 
 function App() {
   return (
     <>
         <Header/>
+        <div style={{height: 50}}/>
+        <NivelRiesgo nivel = {0.1}/>
     </>
 
   );
